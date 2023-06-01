16e3acd41276654ad9eb89580280dc429e5969ea
BaoKunn <baodo200002@gmail.com>
2023-06-02 05:32:36 +0700

    Delete comment code

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -27,34 +27,19 @@ function App() {
           </div>
         </div>
         <div className="box-content">
-          {/* <div className="content-list-item">
-            <h4 className="content-item-title">Lorem ipsum dolor sit asmet?</h4>
-            <div className="content-item">
-              <img
-                className="content-item-img"
-                src="../public/css-icon 1.png"
-                alt=""
-              />
-              <h5 className="content-item-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-                dui sodales, faucibus libero ut, posuere felis. Donec imperdiet
-                suscipit accumsan. Aenean consequat condimentum velit ut tempor.
-                Nam porta massa in metus bibendum congue. Pellentesque ultrices
-                liquam egestas nunc at
-              </h5>
-            </div>
-            <h5 className="content-item-text">
-              ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est
-              ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam
-              porta massa in metus bibendum congue. Pellentesque ultrices
-              vestibulum mattis.
-            </h5>
-          </div> */}
           <ListItem />
         </div>
         <div className="footer">
           <div className="footer-box">
-            <h5 className="footer-copyright">Copyright <img className="footer-icon" src="../public/icon-copyright.png" alt="" /> 2021</h5>
+            <h5 className="footer-copyright">
+              Copyright{" "}
+              <img
+                className="footer-icon"
+                src="../public/icon-copyright.png"
+                alt=""
+              />{" "}
+              2021
+            </h5>
           </div>
         </div>
       </div>
